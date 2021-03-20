5932cde494875c11a86b2af7cc2381a8d81d0ba9
Matthew <elijah2895@hotmail.com>
2021-03-20 12:13:14 -0400

    done

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,25 @@
 class BuildingsController < ApplicationController
+    def index
+        @buildings = Building.all
+    end
+
+    def show
+        #@building = Building.find(params[:id])
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+    def find_building
+        @building = Building.find(params[:id])
+    end
+
+    def building_params
+        params.require(:building).permit
+    end
 end
--- db/migrate/20180704083524_create_offices.rb
+++ db/migrate/20180704083524_create_offices.rb
@@ -1,7 +1,10 @@
 class CreateOffices < ActiveRecord::Migration[5.1]
   def change
     create_table :offices do |t|
-
+      t.string :building
+      t.string :company
+      t.integer :floor
+      
       t.timestamps
     end
   end
